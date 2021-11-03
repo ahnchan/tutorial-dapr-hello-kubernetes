662b5cd8d1c948c34dce44418bf55854aa39eef2
Chanyoung Ahn <ahnchan@gmail.com>
2021-11-03 12:43:20 +0900

    first commit

---
+++ node2/app.js
@@ -0,0 +1,66 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+const express = require('express');
+const bodyParser = require('body-parser');
+require('isomorphic-fetch');
+
+const app = express();
+app.use(bodyParser.json());
+
+// These ports are injected automatically into the container.
+const daprPort = process.env.DAPR_HTTP_PORT; 
+const daprGRPCPort = process.env.DAPR_GRPC_PORT;
+
+const stateStoreName = `statestore`;
+//const stateUrl = `http://localhost:${daprPort}/v1.0/invoke/nodeapp1/method/neworder`;
+const stateUrl = `http://localhost:3501/v1.0/invoke/nodeapp1/method/neworder`;
+
+const port = 3002;
+
+app.get('/status', (_req, res) => {
+    const result = {
+        status: 'ok'
+    }
+    res.send(result);
+});
+
+app.post('/neworder', (req, res) => {
+    const data = req.body.data;
+    const orderId = data.orderId;
+    console.log("Got a new order! Order ID: " + orderId);
+    console.log("url: "+ stateUrl);
+
+    const state = [{
+        key: "order",
+        value: data
+    }];
+
+    fetch(stateUrl, {
+        method: "POST",
+        body: JSON.stringify(state),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw "Failed to new order.";
+        }
+
+        console.log("Successfully new order.");
+        res.status(200).send();
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send({message: error});
+    });
+});
+
+app.get('/ports', (_req, res) => {
+    console.log("DAPR_HTTP_PORT: " + daprPort);
+    console.log("DAPR_GRPC_PORT: " + daprGRPCPort);
+    res.status(200).send({DAPR_HTTP_PORT: daprPort, DAPR_GRPC_PORT: daprGRPCPort })
+});
+
+app.listen(port, () => console.log(`Node App listening on port ${port}!`));
---
+++ nodegateway/app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+
+const app = express();
+
+const port = process.env.APP_PORT || 8080;
+const daprPort = process.env.DAPR_HTTP_PORT || 3500;
+
+const daprUrl = `http://localhost:${daprPort}/v1.0/invoke`;
+// const apiUrl = "http://localhost";
+// const orderUrl = apiUrl + ":3001";
+// const neworderUrl = apiUrl + ":3002";
+
+const orderUrl = "http://localhost:3501/v1.0/invoke/nodeapp1/method";
+const neworderUrl = "http://localhost:3502/v1.0/invoke/nodeapp2/method";
+//const neworderUrl = daprUrl + "/nodeapp2";
+
+// Node1 /order
+app.get('/order', async (req, res) => {
+    // const url = `${daprUrl}/nodeapp1/method/order`;
+    const url = `${orderUrl}/order`;
+    req.pipe(request(url)).pipe(res);
+});
+
+// Node2 /neworder
+app.post('/neworder', async (req, res) => {
+//   const url = `${daprUrl}/nodeapp2/method/newroder`;
+    const url = `${neworderUrl}/neworder`;
+  req.pipe(request(url)).pipe(res);
+});
+
+
+// Gateway status and port informations
+app.get('/gateway/status', async(req, res) => {
+  const result = {
+    status: 'ok'
+  }
+  res.send(result);
+});
+
+app.get('/gateway/status', async(req, res) => {
+  const result = {
+    GATEWAY: port
+  }
+  res.send(result);
+});
+
+
+// Order status and port informations
+app.get('/order/status', async (req, res) => {
+  const url = `${orderUrl}/status`;
+  req.pipe(request(url)).pipe(res);
+});
+
+app.get('/order/ports', async (req, res) => {
+  const url = `${orderUrl}/ports`;
+  req.pipe(request(url)).pipe(res);
+});
+
+// Neworder status and port information
+app.get('/neworder/status', async (req, res) => {
+  const url = `${neworderUrl}/status`;
+  req.pipe(request(url)).pipe(res);
+});
+
+app.get('/neworder/ports', async (req, res) => {
+  const url = `${neworderUrl}/ports`;
+  req.pipe(request(url)).pipe(res);
+});
+
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}!`));
